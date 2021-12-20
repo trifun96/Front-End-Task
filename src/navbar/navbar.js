98ae7ea4aea7526d2e75320bf5415fb1dcd3766f
trifun96 <trifun90@gmail.com>
2021-12-20 14:00:55 +0100

    FrontEnd Task

--- src/navbar/navbar.js
+++ src/navbar/navbar.js
@@ -6,28 +6,23 @@ import { Link } from 'react-router-dom';
 
 class Navbar extends React.Component{
 
-    toggleNavMenu() {
-        const nav = document.getElementById("nav");
-        nav.classList.toggle('active');
-     ;
-    }
+ 
+    
     render() {
         return(
           <div className="navbar" >
-              <ul className="navigation" id="nav">
-                  <Link to="/" onClick={this.toggleNavMenu}>
+              <ul className="navigation">
+                  <Link to="/">
                       <li>Home</li>
                   </Link>
-                  <Link to="/skills" onClick={this.toggleNavMenu}>
+                  <Link to="/skills">
                       <li>Skills</li>
                   </Link>
-                  <Link to="/static" onClick={this.toggleNavMenu}>
+                  <Link to="/static">
                       <li>Static</li>
                   </Link>
               </ul>
-                  <span onClick={this.toggleNavMenu}>
-                      <img src="images/menu.png.png" className='menu-icon'></img>
-                  </span>
+
           </div>
         )
     }
